4f9a0e2dd3b1b19077ed48737869a6f54934bd4a
andrewsuds <andrewsuds@gmail.com>
2022-05-01 05:34:36 -0400

    Added Delete Query

--- index.js
+++ index.js
@@ -24,6 +24,10 @@ types.setTypeParser(types.builtins.NUMERIC, (value) => {
   return parseFloat(value);
 });
 
+types.setTypeParser(types.builtins.INT8, (value) => {
+  return parseInt(value);
+});
+
 // GET
 app.get("/all", (req, res) => {
   pool.query("SELECT * FROM product ORDER BY productID DESC", (err, result) => {
@@ -67,6 +71,21 @@ app.get("/allsell", (req, res) => {
   );
 });
 
+app.get("/statistic", (req, res) => {
+  pool.query(
+    "SELECT SUM(profit) as profit, SUM(qty) as qty FROM sellreceipt;",
+    (err, result) => {
+      if (err) {
+        console.log(err);
+        res.json(err.detail);
+      }
+      if (result) {
+        res.json(result.rows[0]);
+      }
+    }
+  );
+});
+
 // POST
 app.post("/product", (req, res) => {
   const name = req.body.name;
@@ -91,82 +110,95 @@ app.post("/product", (req, res) => {
   );
 });
 
-app.post("/buy", (req, res) => {
+app.post("/buy", async (req, res) => {
   const buyTotal = req.body.buytotal;
   const qty = req.body.qty;
   const productID = req.body.productid;
 
   const buyPrice = buyTotal / qty;
 
-  pool.query(
-    "UPDATE product SET value = value+$1, qtyOnHand = qtyOnHand+$2 WHERE productID = $3;",
-    [buyTotal, qty, productID],
-    (err, result) => {
-      if (err) {
-        console.log(err);
-      }
-    }
-  );
+  try {
+    const result = await pool.query(
+      "INSERT INTO buyreceipt(buyprice, qty, buytotal, date, productid) VALUES($1,$2,$3,CURRENT_DATE,$4) RETURNING buyreceiptid;",
+      [buyPrice, qty, buyTotal, productID]
+    );
 
-  pool.query(
-    "INSERT INTO buyreceipt(buyprice, qty, buytotal, date, productid) VALUES($1,$2,$3,CURRENT_DATE,$4) RETURNING buyreceiptid;",
-    [buyPrice, qty, buyTotal, productID],
-    (err, result) => {
-      if (err) {
-        console.log(err);
-        res.json({ message: err.detail });
-      }
-      if (result) {
-        res.json({
-          buyreceiptid: result.rows[0].buyreceiptid,
-          message: "Success",
-        });
-      }
-    }
-  );
+    await pool.query(
+      "UPDATE product SET value = value+$1, qtyOnHand = qtyOnHand+$2 WHERE productID = $3;",
+      [buyTotal, qty, productID]
+    );
+
+    res.json({
+      buyreceiptid: result.rows[0].buyreceiptid,
+      message: "Success",
+    });
+  } catch (err) {
+    console.log(err);
+    res.json({ message: err.detail });
+  }
 });
 
 app.post("/sell", async (req, res) => {
   const productID = req.body.productid;
   const sellTotal = req.body.selltotal;
   const qty = req.body.qty;
+  let buyPrice;
+  let buyTotal;
+  let sellPrice;
+  let profit;
 
-  const product = await pool.query(
-    "SELECT value, qtyonhand FROM product WHERE productid = $1",
-    [productID]
-  );
+  try {
+    const product = await pool.query(
+      "SELECT value, qtyonhand FROM product WHERE productid = $1",
+      [productID]
+    );
 
-  const buyPrice = product.rows[0].value / product.rows[0].qtyonhand;
-  const buyTotal = (product.rows[0].value / product.rows[0].qtyonhand) * qty;
-  const sellPrice = sellTotal / qty;
-  const profit = sellTotal - buyTotal;
-
-  pool.query(
-    "UPDATE product SET value = value-$1, qtyOnHand = qtyOnHand-$2 WHERE productID = $3;",
-    [buyTotal, qty, productID],
-    (err, result) => {
-      if (err) {
-        console.log(err.detail);
-      }
+    if (product.rowCount > 0) {
+      buyPrice = product.rows[0].value / product.rows[0].qtyonhand;
+      buyTotal = (product.rows[0].value / product.rows[0].qtyonhand) * qty;
+      sellPrice = sellTotal / qty;
+      profit = sellTotal - buyTotal;
+    } else {
+      return res.json({ message: "Product not found" });
     }
-  );
 
-  pool.query(
-    "INSERT INTO sellreceipt(sellprice, buyprice, selltotal, buytotal, profit, qty, date, productID) VALUES($1,$2,$3,$4,$5,$6,CURRENT_DATE,$7) RETURNING sellreceiptid;",
-    [sellPrice, buyPrice, sellTotal, buyTotal, profit, qty, productID],
-    (err, result) => {
-      if (err) {
-        console.log(err);
-        res.json({ message: err.detail });
-      }
-      if (result) {
-        res.json({
-          sellreceiptid: result.rows[0].sellreceiptid,
-          message: "Success",
-        });
-      }
-    }
-  );
+    const result = await pool.query(
+      "INSERT INTO sellreceipt(sellprice, buyprice, selltotal, buytotal, profit, qty, date, productID) VALUES($1,$2,$3,$4,$5,$6,CURRENT_DATE,$7) RETURNING sellreceiptid;",
+      [sellPrice, buyPrice, sellTotal, buyTotal, profit, qty, productID]
+    );
+
+    await pool.query(
+      "UPDATE product SET value = value-$1, qtyOnHand = qtyOnHand-$2 WHERE productID = $3;",
+      [buyTotal, qty, productID]
+    );
+
+    res.json({
+      sellreceiptid: result.rows[0].sellreceiptid,
+      message: "Success",
+    });
+  } catch (err) {
+    console.log(err);
+    res.json({ message: err.detail });
+  }
+});
+
+// DELETE
+app.post("/delproduct", async (req, res) => {
+  const productid = req.body.productid;
+
+  try {
+    await pool.query("DELETE FROM buyreceipt WHERE productid = $1;", [
+      productid,
+    ]);
+    await pool.query("DELETE FROM sellreceipt WHERE productid = $1;", [
+      productid,
+    ]);
+    await pool.query("DELETE FROM product WHERE productid = $1;", [productid]);
+
+    res.json({ message: "Deleted Successfully" });
+  } catch (err) {
+    res.json({ message: err.detail });
+  }
 });
 
 // CREATE
